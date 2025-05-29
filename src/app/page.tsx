93ee0cd157d081dc3f29f4800ddff2a1bf0e7617
Brett Hixon <hixonbm@rose-hulman.edu>
2025-05-28 21:50:08 -0400

    formatting changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import "@/components/HeroBanner"
+import "@/components/HeroBanner";
 import HeroBanner from "@/components/HeroBanner";
 import Filler from "@/components/dev/Filler";
 import BasicPage from "@/components/BasicPage";
@@ -11,10 +11,13 @@ const backgroundColor = "rgb(250, 250, 250)";
 export default function Home() {
   return (
     <BasicPage backgroundColor={backgroundColor}>
-      <div className="flex min-h-screen flex-col items-center px-4" style={ { backgroundColor } }>
-            <HeroBanner />
-            {/* Filler content to enable scrolling */}
-            <Filler />
+      <div
+        className="flex min-h-screen flex-col items-center px-4"
+        style={{ backgroundColor }}
+      >
+        <HeroBanner />
+        {/* Filler content to enable scrolling */}
+        <Filler />
       </div>
     </BasicPage>
   );
