682924d497f5c6c20a92f0312cff55435f90aa8f
Brett Hixon <hixonbm@rose-hulman.edu>
2025-05-29 13:30:49 -0400

    update first project stuffs

--- src/components/Home/SimpleProjectCard.tsx
+++ src/components/Home/SimpleProjectCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardTitle, CardHeader, CardContent, CardDescription } from "@/components/ui/card";
 import { Project } from "@/data/types";
 import formatDate from "@/data/utilities/formatDate";
+import Image from "next/image";
 
 export default function SimpleProjectCard({ project }: { project: Project }) {
   return (
@@ -10,6 +11,15 @@ export default function SimpleProjectCard({ project }: { project: Project }) {
         <CardDescription>{formatDate(project.startYearAndMonth) + " - " + formatDate(project.endYearAndMonth)}</CardDescription>
       </CardHeader>
       <CardContent>
+        <div className="flex justify-center items-center mb-4">
+          <Image
+              src={project.image}
+              alt={project.title}
+              width={200}
+              height={200}
+              className="rounded-lg mb-4"
+          />
+        </div>
         <p>{project.description}</p>
       </CardContent>
     </Card>
