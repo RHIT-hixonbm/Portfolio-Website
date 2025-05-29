38109e40d5c1c29b359afab6d1b4b3b693afef8d
Brett Hixon <hixonbm@rose-hulman.edu>
2025-05-29 10:34:26 -0400

    mobile display tweaks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,27 +7,37 @@ export default function Footer() {
   return (
     <>
       <footer className="w-full max-w-full border-t text-sm text-neutral-500 bg-white rounded-lg border-b border-neutral-200">
-          <div className="grid grid-cols-3 items-center py-4 px-6 shadow-lg rounded-lg w-full max-w-full">
+          <div className="grid grid-cols-3 items-center py-4 px-6 shadow-lg rounded-lg w-full max-w-full gap-x-4">
             {/* Attribution */}
-            <p className="mb-2 md:mb-0">
+            <p className="mb-2 md:mb-0 px-5 text-xs">
               &copy; {new Date().getFullYear()} Brett M. Hixon. Built with Next.js + Tailwind CSS + shadcn/ui.
             </p>
 
-            {/* Back to top button */}
-            <div className="flex justify-center">
+            {/* Call to action buttons */}
+            <div className="flex flex-col items-center md:flex-row justify-center gap-4">
               <Button
                   variant="outline"
                   size="lg"
-                  className="border-neutral-800 text-neutral-800 hover:bg-neutral-800 hover:text-white transition w-max cursor-pointer"
+                  className="border-neutral-800 text-neutral-800 hover:bg-neutral-800 hover:text-white transition w-max cursor-pointer text-xs md:text-sm"
                   aria-label="Back to top"
                   onClick={() => {window.scrollTo({ top: 0, behavior: "smooth" });}}
               > 
                   Back to Top
               </Button>
+              <a href="https://github.com/RHIT-hixonbm/Portfolio-Website" target="_blank" rel="noopener noreferrer">
+                <Button
+                    variant="outline"
+                    size="lg"
+                    className="border-neutral-800 text-neutral-800 hover:bg-neutral-800 hover:text-white transition w-max cursor-pointer text-xs md:text-sm"
+                    aria-label="View source code"
+                > 
+                    View Source Code
+                </Button>
+              </a>
             </div>
 
             {/* Links */}
-            <div className="flex justify-end gap-5 md:gap-10">
+            <div className="flex justify-end gap-3 md:gap-10">
               <a
                 href="mailto:hixonbm@rose-hulman.edu"
                 className="flex items-center space-x-2 hover:text-neutral-900"
