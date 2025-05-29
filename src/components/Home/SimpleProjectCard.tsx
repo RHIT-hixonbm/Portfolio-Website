e00716728d89df8356734413e350d3e8614cf716
Brett Hixon <hixonbm@rose-hulman.edu>
2025-05-29 13:18:26 -0400

    begin adding a real project of mine and add a date formatter

--- src/components/Home/SimpleProjectCard.tsx
+++ src/components/Home/SimpleProjectCard.tsx
@@ -1,12 +1,13 @@
 import { Card, CardTitle, CardHeader, CardContent, CardDescription } from "@/components/ui/card";
 import { Project } from "@/data/types";
+import { parse, format } from "date-fns";
 
 export default function SimpleProjectCard({ project }: { project: Project }) {
   return (
     <Card>
       <CardHeader>
         <CardTitle>{project.title}</CardTitle>
-        <CardDescription>{project.yearAndMonth}</CardDescription>
+        <CardDescription>{format(parse(project.yearAndMonth, 'yyyy-MM', new Date()), 'MMMM yyyy')}</CardDescription>
       </CardHeader>
       <CardContent>
         <p>{project.description}</p>
