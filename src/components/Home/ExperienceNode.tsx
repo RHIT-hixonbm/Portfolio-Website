a42c1ac501b21a2e9f520b200fa4d818e20b60b1
Brett Hixon <hixonbm@rose-hulman.edu>
2025-05-30 16:08:14 -0400

    styling for large screens

--- src/components/Home/ExperienceNode.tsx
+++ src/components/Home/ExperienceNode.tsx
@@ -9,28 +9,41 @@ import {
 } from "@/components/ui/card";
 import { icons } from "lucide-react";
 
-import formatDate from "@/data/utilities/formatDate"; 
+import formatDate from "@/data/utilities/formatDate";
 
-export default function ExperienceNode({ experience }: { experience: Experience }) {
-  const IconComponent = icons[experience.icon as keyof typeof icons] || icons.Briefcase;
+export default function ExperienceNode({
+  experience,
+}: {
+  experience: Experience;
+}) {
+  const IconComponent =
+    icons[experience.icon as keyof typeof icons] || icons.Briefcase;
   return (
-    <Card className="flex flex-col justify-between rounded-full border-4 border-black shadow-lg mt-8 gap-2" style={{ width: "22.7rem", height: "22.7rem" }}>
-        <CardHeader className="mt-3">
-            <CardTitle className="text-center text-md">{experience.title}</CardTitle>
-            <CardDescription className="text-center">
-            <div className="text-xs px-3">{experience.company} - {experience.location}</div>
-            <p className="mt-2 text-sm text-gray-800 text-center">
-                {formatDate(experience.startYearAndMonth)} -{" "}
-                {experience.endYearAndMonth ? formatDate(experience.endYearAndMonth) : "Present"}
-            </p>
-            </CardDescription>
-        </CardHeader>
-        <CardContent className="flex flex-col flex-wrap justify-between items-center pt-4">
-            <p className="mt-2 max-w-3xs text-md text-center">{experience.shortDescription}</p>
-        </CardContent>
-        <CardFooter className="flex flex-col flex-wrap justify-between items-center pt-4">
-            <IconComponent className="text-gray-500" />
-        </CardFooter>
+    <Card className="flex flex-col justify-between rounded-full border-4 border-black shadow-lg mt-8 gap-2 w-[22.7rem] h-[22.7rem] lg:w-[30rem] lg:h-[30rem]">
+      <CardHeader className="mt-3">
+        <CardTitle className="text-center text-md lg:text-2xl">
+          {experience.title}
+        </CardTitle>
+        <CardDescription className="text-center">
+          <div className="text-xs px-3 lg:px-5 lg:text-lg">
+            {experience.company} - {experience.location}
+          </div>
+          <p className="mt-2 text-sm text-gray-800 text-center lg:text-xl">
+            {formatDate(experience.startYearAndMonth)} -{" "}
+            {experience.endYearAndMonth
+              ? formatDate(experience.endYearAndMonth)
+              : "Present"}
+          </p>
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="flex flex-col flex-wrap justify-between items-center pt-4">
+        <p className="mt-2 max-w-3xs lg:max-w-md text-md text-center lg:text-2xl lg:px-5">
+          {experience.shortDescription}
+        </p>
+      </CardContent>
+      <CardFooter className="flex flex-col flex-wrap justify-between items-center pt-4">
+        <IconComponent className="text-gray-500 lg:w-10 lg:h-10" />
+      </CardFooter>
     </Card>
-  )
+  );
 }
