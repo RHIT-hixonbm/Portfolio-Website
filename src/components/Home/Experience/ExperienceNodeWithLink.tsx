0e0266a139ab56a15aed976c6250195f8a56a148
Brett Hixon <hixonbm@rose-hulman.edu>
2025-05-31 07:02:24 -0400

    links fly in now

--- src/components/Home/Experience/ExperienceNodeWithLink.tsx
+++ src/components/Home/Experience/ExperienceNodeWithLink.tsx
@@ -5,12 +5,14 @@ import { useInView } from "react-intersection-observer"
 export default function ExperienceNodeWithLink({experience}: {experience: Experience}) {
     const { ref, inView } = useInView({
         triggerOnce: true,
+        threshold: 0.9,
+        delay: 600,
     })
 
     return (
         <div ref={ref}>
             <span 
-                className={`absolute opacity-0 left-1/2 top-8 -translate-x-1/2 w-2 h-full bg-black ${inView ?? "animate-fly-in"}`}
+                className={`absolute opacity-0 left-1/2 top-8 -translate-x-1/2 w-2 h-full bg-black ${inView && "animate-fly-in"}`}
                 aria-hidden="true"
             />
             <ExperienceNode experience={experience} />
