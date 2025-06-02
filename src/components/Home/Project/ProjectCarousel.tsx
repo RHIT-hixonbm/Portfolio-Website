f70625f2955782ab2ba6e2602c5ad19e6b60bc7c
Brett Hixon <hixonbm@rose-hulman.edu>
2025-06-02 11:48:17 -0400

    switch project carousel to animate when you drag your mouse into it

--- src/components/Home/Project/ProjectCarousel.tsx
+++ src/components/Home/Project/ProjectCarousel.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import { getProjects } from "@/data/data";
 import { Project } from "@/data/types";
 import {
@@ -9,27 +9,47 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import SimpleProjectCard from "./SimpleProjectCard";
-import { useInView } from "react-intersection-observer";
 
 export default function ProjectCarousel() {
-  //Observer to activate animations
-  const { ref, inView } = useInView({
-    delay: 2000,
-    triggerOnce: true,
-    threshold: 0.8,
-    initialInView: false,
-  });
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [hasAnimated, setHasAnimated] = useState(false);
+  const handleMouseEnter = (
+    e: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (!hasAnimated) {
+      setHasAnimated(true);
+      setTimeout(() => {
+        if (ref.current) {
+          ref.current.classList.add("animate-shake");
+
+          ref.current.addEventListener(
+            "animationend",
+            () => {
+              if (ref.current) {
+                ref.current.classList.remove("animate-shake");
+              }
+            },
+            { once: true }
+          );
+        }
+      }, 600);
+    }
+  };
 
   const projects: Project[] = useMemo(() => getProjects(), []);
   return (
     <Carousel className="w-full max-w-2xs xl:max-w-6xl lg:max-w-3xl md:max-w-3xl mb-6">
-      <CarouselContent ref={ref} className={`${inView && "animate-shake" }`}>
-        {projects.filter((project) => project.isFeatured)
-        .map((project) => (
-          <CarouselItem key={project.id}>
-            <SimpleProjectCard project={project} />
-          </CarouselItem>
-        ))}
+      <CarouselContent
+        ref={ref}
+        onMouseEnter={handleMouseEnter}
+      >
+        {projects
+          .filter((project) => project.isFeatured)
+          .map((project) => (
+            <CarouselItem key={project.id}>
+              <SimpleProjectCard project={project} />
+            </CarouselItem>
+          ))}
       </CarouselContent>
       <CarouselPrevious className="border-2 border-black shadow-lg cursor-pointer" />
       <CarouselNext className="border-2 border-black shadow-lg cursor-pointer" />
