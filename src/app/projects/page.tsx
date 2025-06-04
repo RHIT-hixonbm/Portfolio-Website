3a0a286f03b475adc89952ed966b7132c6c5c11d
RHIT-hixonbm <143838816+RHIT-hixonbm@users.noreply.github.com>
2025-06-04 03:03:15 -0400

    add a project page and update page names in tab

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,11 @@
-import AboutComponent from "@/components/About/About";
+import ProjectsComponent from "@/components/Projects/Projects";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Brett M. Hixon | About",
+  title: "Brett M. Hixon | Projects",
   description: "Portfolio website of Brett M. Hixon, showcasing projects and skills in software development.",
 }
 
-export default function About() {
-  return <AboutComponent />
+export default function Projects() {
+    return <ProjectsComponent />
 }
---
+++ src/components/Projects/Projects.tsx
@@ -0,0 +1,5 @@
+export default function ProjectsComponent() {
+    return(
+        <h1 className="mt-20 text-6xl text-center">Full Project Gallery</h1>
+    )
+}
