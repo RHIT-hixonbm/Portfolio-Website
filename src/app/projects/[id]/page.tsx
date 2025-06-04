acff1b886ed6b7f9926a15c7052a8c4aa2988ba6
RHIT-hixonbm <143838816+RHIT-hixonbm@users.noreply.github.com>
2025-06-04 05:34:16 -0400

    fix metadata for details tab name

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,18 +1,27 @@
-"use client";
-
 import ProjectDetailsComponent from "@/components/Projects/ProjectDetails";
 import { getProjects } from "@/data/data";
 import { Project } from "@/data/types";
 import { notFound } from "next/navigation";
-import { useMemo } from "react";
-import { useParams } from 'next/navigation'
 
-export default function ProjectDetails() {
-  const projects = useMemo(() => getProjects(), []);
-  const params = useParams<{ id: string }>();
-  const foundProject = projects.find(
-    (project: Project) => project.id === params.id
+const projects = getProjects();
+function findProject(id: string): Project | undefined {
+  return projects.find(
+    (project: Project) => project.id === id
   );
+}
+export async function generateMetadata({ params }: {params: Promise<{id: string}>}) {
+  const foundProject = findProject((await params).id);
+  if(foundProject) {
+    return {
+      title: `Brett M. Hixon | ${foundProject.title} Details`,
+      description: "Portfolio website of Brett M. Hixon, showcasing projects and skills in software development.",
+    }
+  } 
+} 
+
+export default async function ProjectDetails({ params }: {params: Promise<{id: string}>}) {
+  const syncParams = await params;
+  const foundProject = findProject(syncParams.id);
 
   return foundProject ? (
     <ProjectDetailsComponent project={foundProject} />
