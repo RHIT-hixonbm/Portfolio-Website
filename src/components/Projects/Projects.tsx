9ffa4753286946155b4db5b1453efbc88da233df
RHIT-hixonbm <143838816+RHIT-hixonbm@users.noreply.github.com>
2025-06-04 03:10:37 -0400

    add projects to project page

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,5 +1,19 @@
+import SimpleProjectCard from "../Home/Project/SimpleProjectCard"
+import { Project } from "@/data/types"
+import { getProjects } from "@/data/data"
+import { useMemo } from "react";
+
 export default function ProjectsComponent() {
+    const projects: Project[] = useMemo(() => getProjects(), []);
+
     return(
-        <h1 className="mt-20 text-6xl text-center">Full Project Gallery</h1>
+        <>
+            <h1 className="mt-20 text-6xl text-center">Full Project Gallery</h1>
+            <div className="flex flex-col gap-4 mt-10 px-4">
+                {projects.map((project: Project, idx: number) => {
+                    return <SimpleProjectCard project={project} key={idx} />
+                })}
+            </div>
+        </>
     )
 }
