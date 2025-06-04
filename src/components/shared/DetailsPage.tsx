6c65bc13c157833df4bc2a459c260b1d275ff695
RHIT-hixonbm <143838816+RHIT-hixonbm@users.noreply.github.com>
2025-06-04 05:11:46 -0400

    update video display

--- src/components/shared/DetailsPage.tsx
+++ src/components/shared/DetailsPage.tsx
@@ -18,20 +18,19 @@ export default function DetailsPage({
     <div className="flex flex-col justify-center gap-4">
       <h1 className="text-5xl text-center font-bold mt-20">{title}</h1>
       <div className="flex justify-center">
-        <div className={`relative max-w-sm w-full ${image.aspect} mt-10`}>
-          {videoUrl ? (
-            <iframe
-              width="560"
-              height="315"
-              src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          ) : (
+        {videoUrl ? (
+          <iframe
+            src={videoUrl}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            className="max-w-3xl w-full aspect-video mt-10"
+          />
+        ) : (
+          <div className={`relative max-w-sm w-full ${image.aspect} mt-10`}>
             <Image fill src={image.url} alt={`Image for ${title}`} />
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </div>
   );
