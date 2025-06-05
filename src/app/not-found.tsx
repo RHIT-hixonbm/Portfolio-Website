4e7d6d826bf6ff5e029ef81c670496ad0bfc4892
RHIT-hixonbm <143838816+RHIT-hixonbm@users.noreply.github.com>
2025-06-05 02:32:24 -0400

    add metadata to the 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Brett M. Hixon | 404",
+  description:
+    "Portfolio website of Brett M. Hixon, showcasing projects and skills in software development.",
+};
+
 export default function NotFound() {
-    return (<div className="min-h-screen h-full w-full text-center flex justify-center items-center text-6xl">404 | Not Found</div>)
+  return (
+    <div className="min-h-screen h-full w-full text-center flex justify-center items-center text-6xl">
+      404 | Not Found
+    </div>
+  );
 }
